41a1534ab24e21aa00f7f505fadc9873a3f3dc8b
250349d <root@UBUNTU.>
2025-01-15 17:19:49 +0900

    add widget

--- signup_app/forms.py
+++ signup_app/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 CustomUser = get_user_model()
 
 class SignupForm(UserCreationForm):
+    birthdate = forms.DateField(widget=forms.SelectDateWidget(years=[x for x in range(1900, 2024)]))
     class Meta:
         model = CustomUser 
         fields = ['last_name','first_name', 'email', 'password1', 'password2', 'telephone_number', 'birthdate', 'address', 'street_address'
