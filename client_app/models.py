dd9f7146940109a0a5ffc199bc5596886cdbdb53
root <root@ip-172-31-80-255.ec2.internal>
2025-01-17 03:57:42 +0000

    get_sentinel_user last_name, first_name=None, birthdate=2000-01-01 post_code=0000000 address=None street_address=None email=None@example.com telephone_number=00000000000

--- client_app/models.py
+++ client_app/models.py
@@ -14,11 +14,11 @@ def get_sentinel_user():
 		first_name = "None",
 		last_name = "None",
 		birthdate = '2000-01-01',
-		post_code = "None",
+		post_code = "0000000",
 		address = "None",
 		street_address = "None",
 		email = "none@example.com",
-		telephone_number = "None"
+		telephone_number = "00000000000"
 	)[0]
 
 # Create your models here.
